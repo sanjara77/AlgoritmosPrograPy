a5453a14a08706f0513c4f5384853285b7f7076e
sanjara77 <s.jaramillo7@utp.edu.co>
2026-03-02 19:56:17 -0500

    Update ejercicio8.py

--- ejercicio8.py
+++ ejercicio8.py
@@ -4,23 +4,75 @@ y = 0
 print("Tu posición inicial es X=0, Y=0")
 print("Ingresa 10 números entre 1 y 4")
 
-contador = 0
+# Lanzamiento 1
+numero = int(input("Lanzamiento 1: "))
+if numero == 1: x = x + 1
+if numero == 2: y = y - 1
+if numero == 3: x = x - 1
+if numero == 4: y = y + 1
 
-while contador < 10:
-    numero = int(input("Lanzamiento: "))
+# Lanzamiento 2
+numero = int(input("Lanzamiento 2: "))
+if numero == 1: x = x + 1
+if numero == 2: y = y - 1
+if numero == 3: x = x - 1
+if numero == 4: y = y + 1
 
-    if numero == 1:
-        x = x + 1
-    elif numero == 2:
-        y = y - 1
-    elif numero == 3:
-        x = x - 1
-    elif numero == 4:
-        y = y + 1
-    else:
-        print("Número inválido")
+# Lanzamiento 3
+numero = int(input("Lanzamiento 3: "))
+if numero == 1: x = x + 1
+if numero == 2: y = y - 1
+if numero == 3: x = x - 1
+if numero == 4: y = y + 1
 
-    contador = contador + 1
+# Lanzamiento 4
+numero = int(input("Lanzamiento 4: "))
+if numero == 1: x = x + 1
+if numero == 2: y = y - 1
+if numero == 3: x = x - 1
+if numero == 4: y = y + 1
+
+# Lanzamiento 5
+numero = int(input("Lanzamiento 5: "))
+if numero == 1: x = x + 1
+if numero == 2: y = y - 1
+if numero == 3: x = x - 1
+if numero == 4: y = y + 1
+
+# Lanzamiento 6
+numero = int(input("Lanzamiento 6: "))
+if numero == 1: x = x + 1
+if numero == 2: y = y - 1
+if numero == 3: x = x - 1
+if numero == 4: y = y + 1
+
+# Lanzamiento 7
+numero = int(input("Lanzamiento 7: "))
+if numero == 1: x = x + 1
+if numero == 2: y = y - 1
+if numero == 3: x = x - 1
+if numero == 4: y = y + 1
+
+# Lanzamiento 8
+numero = int(input("Lanzamiento 8: "))
+if numero == 1: x = x + 1
+if numero == 2: y = y - 1
+if numero == 3: x = x - 1
+if numero == 4: y = y + 1
+
+# Lanzamiento 9
+numero = int(input("Lanzamiento 9: "))
+if numero == 1: x = x + 1
+if numero == 2: y = y - 1
+if numero == 3: x = x - 1
+if numero == 4: y = y + 1
+
+# Lanzamiento 10
+numero = int(input("Lanzamiento 10: "))
+if numero == 1: x = x + 1
+if numero == 2: y = y - 1
+if numero == 3: x = x - 1
+if numero == 4: y = y + 1
 
 print("Tu posición final es:")
 print("Posición en X:", x)
