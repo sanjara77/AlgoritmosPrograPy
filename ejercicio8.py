32065e5014f9e7b45aef8a7cf713b63728e2f408
sanjara77 <s.jaramillo7@utp.edu.co>
2026-03-02 20:34:19 -0500

    Update ejercicio8.py

--- ejercicio8.py
+++ ejercicio8.py
@@ -4,75 +4,115 @@ y = 0
 print("Tu posición inicial es X=0, Y=0")
 print("Ingresa 10 números entre 1 y 4")
 
-# Lanzamiento 1
+# Paso 1
 numero = int(input("Lanzamiento 1: "))
-if numero == 1: x = x + 1
-if numero == 2: y = y - 1
-if numero == 3: x = x - 1
-if numero == 4: y = y + 1
+if numero == 1:
+    x = x + 1
+elif numero == 2:
+    y = y - 1
+elif numero == 3:
+    x = x - 1
+elif numero == 4:
+    y = y + 1
 
-# Lanzamiento 2
+# Paso 2
 numero = int(input("Lanzamiento 2: "))
-if numero == 1: x = x + 1
-if numero == 2: y = y - 1
-if numero == 3: x = x - 1
-if numero == 4: y = y + 1
+if numero == 1:
+    x = x + 1
+elif numero == 2:
+    y = y - 1
+elif numero == 3:
+    x = x - 1
+elif numero == 4:
+    y = y + 1
 
-# Lanzamiento 3
+# Paso 3
 numero = int(input("Lanzamiento 3: "))
-if numero == 1: x = x + 1
-if numero == 2: y = y - 1
-if numero == 3: x = x - 1
-if numero == 4: y = y + 1
+if numero == 1:
+    x = x + 1
+elif numero == 2:
+    y = y - 1
+elif numero == 3:
+    x = x - 1
+elif numero == 4:
+    y = y + 1
 
-# Lanzamiento 4
+# Paso 4
 numero = int(input("Lanzamiento 4: "))
-if numero == 1: x = x + 1
-if numero == 2: y = y - 1
-if numero == 3: x = x - 1
-if numero == 4: y = y + 1
+if numero == 1:
+    x = x + 1
+elif numero == 2:
+    y = y - 1
+elif numero == 3:
+    x = x - 1
+elif numero == 4:
+    y = y + 1
 
-# Lanzamiento 5
+# Paso 5
 numero = int(input("Lanzamiento 5: "))
-if numero == 1: x = x + 1
-if numero == 2: y = y - 1
-if numero == 3: x = x - 1
-if numero == 4: y = y + 1
+if numero == 1:
+    x = x + 1
+elif numero == 2:
+    y = y - 1
+elif numero == 3:
+    x = x - 1
+elif numero == 4:
+    y = y + 1
 
-# Lanzamiento 6
+# Paso 6
 numero = int(input("Lanzamiento 6: "))
-if numero == 1: x = x + 1
-if numero == 2: y = y - 1
-if numero == 3: x = x - 1
-if numero == 4: y = y + 1
+if numero == 1:
+    x = x + 1
+elif numero == 2:
+    y = y - 1
+elif numero == 3:
+    x = x - 1
+elif numero == 4:
+    y = y + 1
 
-# Lanzamiento 7
+# Paso 7
 numero = int(input("Lanzamiento 7: "))
-if numero == 1: x = x + 1
-if numero == 2: y = y - 1
-if numero == 3: x = x - 1
-if numero == 4: y = y + 1
+if numero == 1:
+    x = x + 1
+elif numero == 2:
+    y = y - 1
+elif numero == 3:
+    x = x - 1
+elif numero == 4:
+    y = y + 1
 
-# Lanzamiento 8
+# Paso 8
 numero = int(input("Lanzamiento 8: "))
-if numero == 1: x = x + 1
-if numero == 2: y = y - 1
-if numero == 3: x = x - 1
-if numero == 4: y = y + 1
+if numero == 1:
+    x = x + 1
+elif numero == 2:
+    y = y - 1
+elif numero == 3:
+    x = x - 1
+elif numero == 4:
+    y = y + 1
 
-# Lanzamiento 9
+# Paso 9
 numero = int(input("Lanzamiento 9: "))
-if numero == 1: x = x + 1
-if numero == 2: y = y - 1
-if numero == 3: x = x - 1
-if numero == 4: y = y + 1
+if numero == 1:
+    x = x + 1
+elif numero == 2:
+    y = y - 1
+elif numero == 3:
+    x = x - 1
+elif numero == 4:
+    y = y + 1
 
-# Lanzamiento 10
+# Paso 10
 numero = int(input("Lanzamiento 10: "))
-if numero == 1: x = x + 1
-if numero == 2: y = y - 1
-if numero == 3: x = x - 1
-if numero == 4: y = y + 1
+if numero == 1:
+    x = x + 1
+elif numero == 2:
+    y = y - 1
+elif numero == 3:
+    x = x - 1
+elif numero == 4:
+    y = y + 1
 
 print("Tu posición final es:")
 print("Posición en X:", x)
