4aad6b18dbfb389dd1c4ee06f21912fcb6d34a99
sanjara77 <s.jaramillo7@utp.edu.co>
2026-03-09 14:33:21 -0500

    Delete ejercicio1lista.py

--- ejercicio1lista.py
+++
@@ -1,8 +0,0 @@
-print("bienvenido usuario")
-list = ["pepe", "luis", "ana", "pablo", "albaro"]
-
-name = input("ingrese su nombre: ")
-if name in list:
-    print("bienvenido conductor")
-else:
-    print("vayase de aqui")
